312b8830045bba88409dd9ed13381594c984a138
Aiden Albertson <ajalber@pointpark.edu>
2026-02-04 22:08:26 -0500

    Update dev

--- deploy-main.bat
+++ deploy-main.bat
@@ -1,16 +1,6 @@
 @echo off
-
-echo Switching to main branch...
 git checkout main
-
-echo Merging dev into main...
-git merge dev
-
-echo Pushing main branch to GitHub...
+git add .
+git commit -m "Deploy update"
 git push origin main
-
-echo Switching back to dev branch...
-git checkout dev
-
-echo Deployment complete!
 pause
